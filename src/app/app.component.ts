1ba713506af8c6877cb0b144580f1dd6e4b19841
Nicolas Sanchez <nsanchezl@chilexpress.cl>
2024-04-19 11:31:06 -0400

    Update app.component.css and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavComponent } from './nav/nav.component';
 import { UserinfoComponent } from './userinfo/userinfo.component';
 import { HeaderComponent } from './header/header.component';
 import { PostsComponent } from './posts/posts.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,UserinfoComponent,HeaderComponent,PostsComponent],
+  imports: [RouterOutlet,NavComponent,UserinfoComponent,HeaderComponent,PostsComponent,CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
